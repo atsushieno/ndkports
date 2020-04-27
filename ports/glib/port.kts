889fcb47aab267401724783ac5341e48fc5af8c0
atsushieno <atsushieno@gmail.com>
2020-04-27 15:12:34 +0900

    remove extraneous comments and wrong copyright assignments.

--- ports/glib/port.kts
+++ ports/glib/port.kts
@@ -1,26 +1,7 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.ndkports
 
 import java.io.File
 
-// The port is based on Cerbero libffi recipe + patch.
-// https://gitlab.freedesktop.org/gstreamer/cerbero/-/tree/master/recipes
-// Note that the latest libffi is 3.3 and 3.3-rc0 is > 1 year behind.
 object : MesonPort() {
     override val name = "glib"
     override val version = "2.60.6"
